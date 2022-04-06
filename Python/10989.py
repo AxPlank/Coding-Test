d5cc9833f304a951ee471cf21bfe21dae7a93649
Papus <foal0912@gmail.com>
2022-04-06 15:48:24 +0900

    4월 6일 코딩테스트 연습

--- Python/10989.py
+++ Python/10989.py
@@ -1,26 +1,13 @@
 import sys
-import time
-import random
 
-# N = int(sys.stdin.readline().strip())
+N = int(sys.stdin.readline())
+num_input = [0] * 10000
 
-num_list = []
-# for i in range(N):
-#     num = int(sys.stdin.readline().strip())
-#     num_list.append(num)
-    
-for i in range(10000000):
-    num_list.append(random.randint(1, 10001))
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    num_input[num-1] += 1
 
-sort_list = []
-start = time.time()
-for i in range(1, 10001):
-    if len(sort_list) == len(num_list):
-        break
-    if i not in num_list:
-        continue
-    sort_list += [i] * num_list.count(i)
-    
-for i in sort_list:
-    print(i)
-print(start - time.time())
+for i in range(10000):
+    if num_input[i] != 0:
+        for _ in range(num_input[i]):
+            print(i)
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 listt = [1, 2, 3, 4, 4, 5, 5, 4, 2]
-for i in range(len(listt)):
-    print(listt.pop())
+
+for i in sorted(listt):
+    print(i)
